9b99174b826c1037313fe16ad81e29967ec3d5ea
borojetski <borojetski@gmail.com>
2022-04-10 17:24:40 -1000

    Setup first api example for c26.

---
+++ class26/api-ex-1/js/main.js
@@ -0,0 +1,18 @@
+// 
+
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', searchForSoulsilver)
+
+function searchForSoulsilver(){
+  const choice = document.querySelector('input').value
+  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
