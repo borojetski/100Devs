e2127bdf23e4bdc6994c8ff7a6d0d969a6b47cc1
borojetski <borojetski@gmail.com>
2022-04-10 23:24:21 -1000

    Create a VPN checker site to determine whether VPN is on or off.

--- class26/api-ex-1/js/main.js
+++ class26/api-ex-1/js/main.js
@@ -1,16 +1,22 @@
-// 
+// Query the freegeoip api to determine whether your current ip address is based in California.
+document.querySelector('button').addEventListener('click', isMyVpnOn)
 
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', searchForSoulsilver)
-
-function searchForSoulsilver(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function isMyVpnOn(){
+  const url = 'https://api.freegeoip.app/json/?apikey=65280b00-b974-11ec-8f2b-734bbb0b7ae8'
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
+        console.log(data.ip)
+        console.log(data.region_name)
+        function region(){
+          if(data.region_name === "California"){
+            return 'OFF!'
+          } "On."
+        }
+        document.querySelector('h3').innerText = `Your IP Address Region is ${data.region_name}.`
+        // document.querySelector('img').src = data.drinks[0].strDrinkThumb
+        document.querySelector('h2').innerText = `Your VPN is ${region()}`
       })
       .catch(err => {
           console.log(`error ${err}`)
