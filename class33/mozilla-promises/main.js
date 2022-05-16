1e182ad2bca830dfebdd12d7012974e9f942996f
borojetski <borojetski@gmail.com>
2022-05-15 21:12:47 -1000

    Complete Mozilla promises lecture.

--- class33/mozilla-promises/main.js
+++ class33/mozilla-promises/main.js
@@ -17,15 +17,91 @@
 //   })
 // })
 
-const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
+// const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
 
-fetchPromise
-  .then( response => {
-    if (!response.ok){
+// fetchPromise
+//   .then( response => {
+//     if (!response.ok){
+//       throw new Error(`HTTP error: ${response.status}`)
+//     }
+//     return response.json()
+//   })
+//   .then ( json => {
+//     console.log(json[0].name)
+//   })
+
+// const fetchPromise = fetch('bad-scheme://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
+
+// fetchPromise
+//   .then( response => {
+//     if (!response.ok){
+//       throw new Error(`HTTP error: ${response.status}`)
+//     }
+//     return response.json()
+//   })
+//     .then( json => {
+//       console.log(json[0].name)
+//     })
+//     .catch ( error => {
+//       console.error(`Could not get products: ${error}`)
+//     })
+
+// const fetchPromise1 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
+// const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found')
+// const fetchPromise3 = fetch('bad-scheme://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json')
+
+// Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+//     .then( responses => {
+//       for (const response of responses){
+//         console.log(`${response.url}: ${response.status}`)
+//       }
+//     })
+//     .catch(error => {
+//       console.error(`Failed to fetch: ${error}`)
+//     })
+
+// const fetchPromise1 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
+// const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found')
+// const fetchPromise3 = fetch('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json')
+
+// Promise.any([fetchPromise1, fetchPromise2, fetchPromise3])
+//   .then(response => {
+//     console.log(`${response.url}: ${response.status}`)
+//   })
+//   .catch( error => {
+//     console.error(`Failed to fetch: ${error}`)
+//   })
+
+// async function fetchProducts(){
+//   try {
+//     const response = await fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
+//     if (!response.ok){
+//       throw new Error(`HTTP error: ${response.status}`)
+//     }
+//     const json = await response.json()
+//     console.log(json[0].name)
+//   }
+//   catch(error){
+//     console.error(`Could not get products: ${error}`)
+//   }
+// }
+
+// fetchProducts()
+
+async function fetchProducts(){
+  try {
+    const response = await fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
+    if(!response.ok){
       throw new Error(`HTTP error: ${response.status}`)
     }
-    return response.json()
-  })
-  .then ( json => {
-    console.log(json[0].name)
-  })
+    const json = await response.json()
+    return json
+  }
+  catch(error) {
+    console.error(`Could not get products: ${error}`)
+  }
+}
+
+const jsonPromise = fetchProducts()
+jsonPromise.then((json) => console.log(json[0].name))
+
